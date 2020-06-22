9e2f1cdd6d483165569a57010a1025a60febec78
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-06-22 15:08:00 -0300

    WebAPI

---
+++ WAPI/WebAPI/WebAPI/Models/Clientes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Clientes
+    {
+        public int id { get; set; }
+
+        public string Nome { get; set; }
+
+        public Clientes(string texto)
+        {
+            this.Nome = texto;
+        }
+    }
+}
