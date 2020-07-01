6e7e62ac2b5a3ecb96603a56de37f822e25e3261
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-06-30 23:55:23 -0300

    Teste-XamarinForms

---
+++ teste-app/ExemploView/ExemploView/ExemploView/Views/ExemploView3.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExemploView.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ExemploView3 : ContentView
+    {
+        public ExemploView3()
+        {
+            InitializeComponent();
+        }
+    }
+}
